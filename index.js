66646b442b769bdb6eb9034fc0e209ae7b6aa427
Geovanna Andrade dos Santos <geovanna1544@icloud.com>
2026-09-01 16:18:42 -0300

    feat: adiciona arquivo index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
-const calculadora = require ('./calculadora'); 
-const bhaskara = require ('./bhaskara'); 
-console.log ("===================================") ; 
-console.log ("[INÍCIO] SISTEMA MATEMÁTICO INICIADO [INÍCIO]"); 
-console.log ("===================================\ n"); 
+const calculadora = require('./calculadora'); 
+const bhaskara = require('./bhaskara'); 
+console.log("===================================") ; 
+console.log("[INÍCIO] SISTEMA MATEMÁTICO INICIADO [INÍCIO]"); 
+console.log("===================================\ n"); 
 
 console.log("--- TESTANDO A CALCULADORA ---"); 
 console.log(`Soma de 10 + 5 = ${calculadora.somar(10, 5)}`); 
@@ -12,6 +12,6 @@ console.log(` Multiplicacao de 4 * 6 = ${calculadora.multiplicar(4, 6)}`);
 console.log(`Divisao de 15 / 3 = ${calculadora.dividir(15, 3)}`); 
 console.log(`Teste erro divisao = ${calculadora.dividir(10, 0)}`); 
 console.log ("\n--- TESTANDO O BHASKARA ---"); 
-
+const resultado = bhaskara(1, -5, 6);
 console.table(resultado);
-console.log ("\n[SUCESSO] Sistema executado com sucesso !\n");
+console.log("\n[SUCESSO] Sistema executado com sucesso !\n");
