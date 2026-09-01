401764a42a50d2a21dac735125a648c8836d9f03
Clarice Pietra <claricepietra7@gmail.com>
2026-09-01 14:26:55 -0300

    feat: finaliza o modulo de funcionalidade matematica

---
+++ bhaskara.js
@@ -0,0 +1,17 @@
+function calculateBhaskara(a, b, c) {
+    if (a === 0) {
+        return "O valor de 'a' não pode ser zero em uma equação de 2º grau.";
+    }
+    const delta = ( b * b ) - ( 4 * a * c);
+    if (delta < 0) {
+        return "Delta negativo. Não existem raízes reais.";
+    }
+    const x1 = (-b + Math.sqrt(delta)) / (2 * a);
+    const x2 = (-b - Math.sqrt(delta)) / (2 * a);
+    return {
+        delta: delta,
+        x1: x1,
+        x2: x2
+    };
+}
+module.exports = calcularBhaskara ;
---
+++ calculadora.js
@@ -0,0 +1,22 @@
+function soma(a,b) {
+    return a + b;
+}
+
+function subtrair(a,b) {
+    return a - b;
+}
+function multiplicar(a,b) {
+    return a * b;
+}
+function dividir(a,b) {
+    if (b === 0) {
+        return "Erro: Divisão por zero não é permitida.";
+    }
+    return a / b;
+}
+module.exports = {
+somar ,
+subtrair ,
+multiplicar ,
+dividir
+};
